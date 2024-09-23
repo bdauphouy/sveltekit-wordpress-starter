5ff0f97b842170510213134cec213339d024fce2
Baptiste Dauphouy <bdauphouy@gmail.com>
2024-09-24 01:36:12 +0200

    initial commit

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+	watchSchema: {
+		url: 'http://wordpress/graphql'
+	},
+	plugins: {
+		'houdini-svelte': {}
+	}
+};
+
+export default config;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="flex h-screen flex-col items-center justify-center gap-4">
+	<h1>Welcome to this SvelteKit with WordPress starter.</h1>
+</div>
